2e11f838d6cd17c97aa8c3ae51b2636cb0896878
SaurabhBagde <bagdesaurabh69@gmail.com>
2021-01-02 19:06:36 +0530

    shortcode part two

---
+++ admin/book-shortcode.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * @link       https://github.com/SaurabhBagde
+ * @since      1.0.0
+ *
+ * @package    Wp_Book
+ * @subpackage Wp_Book/admin/book-shortcode
+ */
+
+
